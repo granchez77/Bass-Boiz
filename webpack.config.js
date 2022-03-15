a997d0bdc3d45119a04dc38b409dc799b4c6a187
Grant Sanchez <granchez77@gmail.com>
2022-03-15 11:42:14 -0700

    created notes object

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, "client/src", "index.js"),
+  entry: path.join(__dirname, "client/src", "index.jsx"),
   output: {
     path:path.resolve(__dirname, "client/dist"),
   },
